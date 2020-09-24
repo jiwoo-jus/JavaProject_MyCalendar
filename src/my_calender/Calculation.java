6fab8247464a8fe9dbda90d363b3fdd2df48395e
Park, Jiwoo <parkwjioo9980@gmail.com>
2020-09-25 06:57:17 +0900

    윤년 계산 구현 완료. 모두 정확한 결과값 나옴.

--- src/my_calender/Calculation.java
+++ src/my_calender/Calculation.java
@@ -27,18 +27,32 @@ public class Calculation {
 		int new_day = Integer.parseInt(newdate[2]);
 		Calendar cal = Calendar.getInstance();
 		int [] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		int s1 = 0, s2 = 0, total = 0;
+		int leap_year_count = 0, s1 = 0, s2 = 0, total = 0;
 
-		for(int j = new_month; j > 0; j--)
-			s1 += months[j-1];
-		s1 += new_day;
-		for(int j = cal.get(Calendar.MONTH); j > 0; j--)
-			s2 += months[j-1];
+		//현재년도부터 미래년도 사이에 있는 윤년 개수 세기
+		for(int i = cal.get(Calendar.YEAR); i < new_year; i++) {
+			if((i%4 == 0 && i%100 != 0) || i%400 == 0)
+				leap_year_count++;
+		}
+		//s1 = 미래 날짜의 월+일 일수
+		for(int i = new_month; i > 0; i--) { //ex-a)xx년 5월 14일이면 이곳에선 1,2,3,4월 누적 일수 더함: 31+(28)+31+30
+			if(i==1 && (new_year%4 == 0 && new_year % 100 != 0) || new_year%400 == 0) //2월 누적 일수를 더할 때 해당 월이 윤달인지 확인
+				s1++;
+			s1 += months[i-1];
+		}
+		s1 += new_day; //ex-a)이곳에선 앞서 구한 것에 14일을 더함: 31+(28)+31+30+'14'
+		//s2 = 현재 날짜의 월+일 일수
+		for(int i = cal.get(Calendar.MONTH); i > 0; i--) {
+			if(i==1 && (cal.get(Calendar.YEAR)%4 == 0 && cal.get(Calendar.YEAR)%100 != 0) || cal.get(Calendar.YEAR)%400 == 0)
+				s2++;
+			s2 += months[i-1];
+		}
 		s2 += cal.get(Calendar.DAY_OF_MONTH);
+		//s1이 s2보다 작으면 1년(365일)땡겨 써서 월+일 일수 차이를 양수로 계산, 대신 년도 차이 구할 때 하나 차감
 		if(s1 > s2)
-			total = (new_year - cal.get(Calendar.YEAR)) * 365 + (s1 - s2);
+			total = (new_year - cal.get(Calendar.YEAR)) * 365 + leap_year_count + (s1 - s2);
 		else
-			total = (new_year - cal.get(Calendar.YEAR) - 1) *365 + (s1 + 365 - s2);
+			total = (new_year - cal.get(Calendar.YEAR) - 1) *365 + leap_year_count + (s1 + 365 - s2);
 
 		System.out.print("=> 오늘로부터 " + total + "일 후 입니다.");
 		System.out.print("(" + total/365 + "년  그리고 ");
